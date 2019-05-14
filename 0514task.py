bce4d85cb5a00b4712023c8b1b01ba0f9b7b99a7
11Zero <haitao.tian.china@gmail.com>
2019-05-14 23:02:49 +0800

    abaqus操作odb

---
+++ 0514task.py
@@ -0,0 +1,27 @@
+from abaqus import *
+from abaqusConstants import *
+from caeModules import *
+from driverUtils import executeOnCaeStartup
+import sys
+sys.path.insert(9, r'd:/Program Files/abaqus/6.14-1/code/python2.7/lib/abaqus_plugins/odbCombine') # 这句是引入dob操作库，路径要改为自己的软件路径
+import odbCombineKernel
+odbName = 'D:/Program Files/abaqus/Temp/Job-1.odb' # odb文件位置
+stepName = 'Step-1' #需要输出的分析步
+E=300000000 # 材料弹性模量
+o1 = session.openOdb(name=odbName)
+session.viewports['Viewport: 1'].setValues(displayedObject=o1)
+s1f1_E = session.odbs[odbName].steps[stepName].frames[-1].fieldOutputs['E']
+tmpField = s1f1_E.getScalarField(
+    invariant=MAX_PRINCIPAL)*s1f1_E.getScalarField(
+    invariant=MAX_PRINCIPAL)*0.5*300000000
+currentOdb = session.odbs[odbName]
+scratchOdb = session.ScratchOdb(odb=currentOdb)
+sessionStep = scratchOdb.Step(name='Session Step', 
+    description='Step for Viewer non-persistent fields', domain=TIME, 
+    timePeriod=1.0)
+sessionFrame = sessionStep.Frame(frameId=0, frameValue=0.0, 
+    description='Session Frame')
+sessionField = sessionFrame.FieldOutput(name='Field-needed', 
+    description='s1f1_E.getScalarField(invariant=MAX_PRINCIPAL)*s1f1_E.getScalarField(invariant=MAX_PRINCIPAL)*0.5*'+str(E), 
+    field=tmpField)
+
